41d0fd961bef372c621181f998c15cea307d16aa
khalidjoda03-sys <khalidjoda03@gmail.com>
2026-07-21 01:27:00 +0300

    Fix onboarding widget smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:majalisna/app.dart';
 import 'package:majalisna/core/constants/app_strings.dart';
+import 'package:majalisna/navigation/app_routes.dart';
 
 void main() {
-  testWidgets('Majalisna app boots to splash', (tester) async {
-    await tester.pumpWidget(const MajalisnaApp());
+  testWidgets('Majalisna app boots to onboarding', (tester) async {
+    await tester.pumpWidget(
+      const MajalisnaApp(initialRoute: AppRoutes.onboarding),
+    );
     await tester.pump();
 
     expect(find.byType(MaterialApp), findsOneWidget);
